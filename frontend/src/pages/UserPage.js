073640954daf8515461bb2fad67ae3f9fd291730
RNC81 <rayaannathoo@gmail.com>
2025-11-07 15:02:49 +0100

    log_invite

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { getGuestCode, regenerateGuestCode, getGuestLogs } from '../services/api'; // 1. Importer getGuestLogs
 import { Button } from '../components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card';
+import { Badge } from '../components/ui/badge';
 import {
   Table,
   TableBody,
